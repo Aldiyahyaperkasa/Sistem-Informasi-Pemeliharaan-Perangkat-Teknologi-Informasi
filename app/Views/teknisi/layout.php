7b0c84ba69af196e294aa000146e5defa21a3aa4
aldiyahyaperkasa <aldiyahyap@gmail.com>
2024-12-09 00:23:59 +0800

    mengubah kolom database yang keliru dan mengubah icon tab di website

--- app/Views/teknisi/layout.php
+++ app/Views/teknisi/layout.php
@@ -4,6 +4,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="<?= base_url() ?>image/logomurni.png" type="image/x-icon">
+
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
